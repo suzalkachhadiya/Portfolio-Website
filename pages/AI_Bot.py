050ca48cb9285c52b3f939e3b3aff64b91dbe625
suzal kachhadiya <suzalkachhadiya111@gmail.com>
2025-11-01 09:26:39 +0530

    model and resume updated

--- pages/AI_Bot.py
+++ pages/AI_Bot.py
@@ -8,7 +8,7 @@ st.set_page_config(
 )
 api_key=st.secrets["API_KEY"]
 genai.configure(api_key=api_key)
-model=genai.GenerativeModel("gemini-1.5-flash")
+model=genai.GenerativeModel("gemini-2.5-flash")
 
 
 persona="""
